2ab02e385cc5d50fadd7599b847f4001b81a61b1
Artyom <artemius16071998@gmail.com>
2019-11-25 23:11:28 +0300

    working resume

--- MediaWatcher/Media.cs
+++ MediaWatcher/Media.cs
@@ -3,11 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace MediaWatcher
 {
-    class Media
+    public class Media
     {
         public string Title { get; set; }
+    }
+
+    public class VideoDataObject : Media
+    {
+        public string VideoLocation { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+
+        public VideoProperties videoProperties { get; set; }
+        public BitmapImage Display { get; set; }
+        public StorageFile VideoFile { get; set; }
+
+        public VideoDataObject(StorageFile file)
+        {
+            VideoFile = file;
+            InitializeVideo();
+        }
+
+        private async void InitializeVideo()
+        {
+            Title = VideoFile.DisplayName;
+            videoProperties = await VideoFile.Properties.GetVideoPropertiesAsync();
+            var dur = videoProperties.Duration;
+            Duration = dur.Hours.ToString() + " : " + dur.Minutes.ToString() + " : " + dur.Seconds.ToString();
+            Display = await GetDisplay();
+        }
+
+        private async Task<BitmapImage> GetDisplay()
+        {
+            var bitMap = new BitmapImage();
+            using (var imgSource = await VideoFile.GetScaledImageAsThumbnailAsync(ThumbnailMode.VideosView))
+            {
+                if (imgSource != null)
+                    bitMap.SetSource(imgSource);
+                else
+                {
+                    var storageFolder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+                    var storageFile = await storageFolder.GetFileAsync("Logo.png");
+                    bitMap.UriSource = new Uri(storageFile.Path);
+                }
+            }
+            return bitMap;
+        }
     }
 }
--- MediaWatcher/VideosWatcher.xaml.cs
+++ MediaWatcher/VideosWatcher.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Storage.FileProperties;
@@ -7,53 +10,41 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace MediaWatcher
 {
-    public class VideoDataObject{
-        public string Title { get; set; }
-        public string VideoLocation { get; set; }
-        public string Description { get; set; }
-        public string Duration { get; set; }
-
-        public VideoProperties videoProperties { get; set; }
-        public BitmapImage Display { get; set; }
-        public StorageFile VideoFile { get; set; }
-
-        public VideoDataObject(StorageFile file) {
-            VideoFile = file;
-            Initialize();
-        }
-
-        private async void Initialize()
-        {
-            Title = VideoFile.DisplayName;
-            videoProperties = await VideoFile.Properties.GetVideoPropertiesAsync();
-            var dur = videoProperties.Duration;
-            Duration = dur.Hours.ToString() + " : " + dur.Minutes.ToString() + " : " + dur.Seconds.ToString();
-            Display = await GetDisplay();
-        }
-
-        private async Task<BitmapImage> GetDisplay()
-        {
-            var bitMap = new BitmapImage();
-            using (var imgSource = await VideoFile.GetScaledImageAsThumbnailAsync(ThumbnailMode.VideosView))
-            {
-                if (imgSource != null)
-                    bitMap.SetSource(imgSource);
-                else
-                {
-                    var storageFolder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
-                    var storageFile = await storageFolder.GetFileAsync("Logo.png");
-                    bitMap.UriSource = new Uri(storageFile.Path);
-                }
-            }
-            return bitMap;
-        }
-    }
-
     public sealed partial class VideosWatcher : Page
     {
         public VideosWatcher()
         {
             this.InitializeComponent();
+        }
+    }
+
+    public class VideoWatcherViewModel
+    {
+        public VideoWatcherViewModel(StorageFolder stFolder)
+        {
+            VideoItems = new ObservableCollection<VideoDataObject>();
+            MainFolder = stFolder;
+        }
+
+        public ObservableCollection<VideoDataObject> VideoItems { get; set; }
+
+        private StorageFolder MainFolder;
+        private IEnumerable<StorageFile> Videos;
+        private char[] sep = new char[] { '/' };
+
+        private async void Initialize()
+        {
+            Videos = await MainFolder.GetFilesAsync();
+            Videos = Videos.Where(a => a.ContentType.Split(sep)[0] == "video");
+            FillUp();
+        }
+
+        private void FillUp()
+        {
+            foreach(var file in Videos)
+            {
+                VideoItems.Add(new VideoDataObject(file));
+            }
         }
     }
 }
